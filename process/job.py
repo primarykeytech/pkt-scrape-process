289c945eb128d662dc1fd063d63aaeecb5f3746c
John Wagoner <john@primarykeytech.com>
2024-03-06 22:41:52 -0800

    PySpark poc in pycharm.

---
+++ process/job.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+import os
+
+
+def init_spark():
+    """
+    Initialize the spark session.
+    :return: SparkSession
+    """
+    return (SparkSession.builder
+            .appName("pkt-scrape-process")
+            .config("spark.memory.offHeap.enabled", "true")
+            .config("spark.memory.offHeap.size", "10g").getOrCreate())
+
+
+def main():
+    spark = init_spark()
+    df = spark.read.csv('online_retail.csv', header=True,escape="\"")
+    df.show()
+    print(df.count())
+
+
+if __name__ == '__main__':
+    main()
