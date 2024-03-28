e5b4f516896866b7c5f408c598f7de9b2d62adb6
John Wagoner <john@primarykeytech.com>
2024-03-27 23:17:58 -0700

    Finished db unit tests.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,4 @@
 import pytest
-from moto import mock_aws
 from pktscrape.module_db import DynamoDb
 from pktscrape.module_scraping import Experience
 from fixtures import data_table, data_table_with_data
@@ -34,61 +33,83 @@ def test_create_record(data_table, data_table_with_data):
     assert obj_item.classification == "aspirin"
 
 
+def test_create_multiple_records(data_table, data_table_with_data):
+    """
+    Test the create_multiple_records function.
+    :param data_table: mock table.
+    :param data_table_with_data: mock table with data.
+    """
+    # create an object and set the values.
+    obj_exp1 = Experience()
+    obj_exp1.uuid = "11111111-1111-2222-3333-44444444444444"
+    obj_exp1.title = "Jane Smith Experience"
+    obj_exp1.classification = "aspirin"
+    obj_exp1.description = "This is a test description"
 
-# @mock_aws
-# def test_create_multiple_records():
-#
-#     # create an object and set the values.
-#     obj_exp1 = Experience()
-#     obj_exp1.uuid = "11111111-1111-2222-3333-44444444444444"
-#     obj_exp1.title = "Jane Smith Experience"
-#     obj_exp1.classification = "aspirin"
-#     obj_exp1.description = "This is a test description"
-#
-#     # create an object and set the values.
-#     obj_exp2 = Experience()
-#     obj_exp2.uuid = "22222222-1111-2222-3333-44444444444444"
-#     obj_exp2.title = "Baby Smith Experience"
-#     obj_exp2.classification = "aspirin"
-#     obj_exp2.description = "This is a test description"
-#
-#     # list to hold the objects.
-#     list_obj_exp = [obj_exp1, obj_exp2]
-#
-#     # using db object.
-#     obj_db = DynamoDb()
-#
-#     # create the records.
-#     obj_db.create_multiple_records(list_obj_exp)
-#
-#     retrieved_exp1 = obj_db.read_one_by_id("11111111-1111-2222-3333-44444444444444")
-#     retrieved_exp2 = obj_db.read_one_by_id("22222222-1111-2222-3333-44444444444444")
-#
-#
-# @mock_aws
-# def test_retrieve_record():
-#
-#     # set the id to be the same as the create record test.
-#     db_id = '00000000-1111-2222-3333-44444444444444'
-#
-#     # create the dynamo db object.
-#     obj_db = DynamoDb()
-#
-#     # get the result as experience obj for this id.
-#     obj_result = obj_db.read_one_by_id(db_id)
-#
-#     # run the test
-#     assert obj_result.title == "John Smith Experience"
-#
-#
-# @mock_aws
-# def test_delete_record():
-#
-#     # set the id to be the same as the create record test.
-#     db_id = '00000000-1111-2222-3333-44444444444444'
-#
-#     # create the dynamo db object.
-#     obj_db = DynamoDb()
-#
-#     # run the test
-#     assert obj_db.delete_by_id(db_id) is True
+    # create an object and set the values.
+    obj_exp2 = Experience()
+    obj_exp2.uuid = "22222222-1111-2222-3333-44444444444444"
+    obj_exp2.title = "Baby Smith Experience"
+    obj_exp2.classification = "aspirin"
+    obj_exp2.description = "This is a test description"
+
+    # list to hold the objects.
+    list_obj_exp = [obj_exp1, obj_exp2]
+
+    # using db object.
+    obj_db = DynamoDb()
+
+    # overwrite the table with the mock table.
+    obj_db.table = data_table_with_data
+
+    # create the records.
+    obj_db.create_multiple_records(list_obj_exp)
+
+    # get the records from the mock table.
+    retrieved_exp1 = obj_db.read_one_by_id("11111111-1111-2222-3333-44444444444444")
+    retrieved_exp2 = obj_db.read_one_by_id("22222222-1111-2222-3333-44444444444444")
+
+    # run the tests.
+    assert retrieved_exp1.title == "Jane Smith Experience"
+    assert retrieved_exp2.title == "Baby Smith Experience"
+
+
+def test_retrieve_record(data_table, data_table_with_data):
+    """
+    Test the read_one_by_id function.
+    :param data_table: mock table.
+    :param data_table_with_data: mock table with data.
+    """
+    # set the id to be the same as the mock table in the fixture.
+    db_id = 'abcd-1234'
+
+    # create the dynamo db object.
+    obj_db = DynamoDb()
+
+    # overwrite the table with the mock table.
+    obj_db.table = data_table_with_data
+
+    # get the result as experience obj for this id.
+    obj_result = obj_db.read_one_by_id(db_id)
+
+    # run the test
+    assert obj_result.title == "title1"
+
+
+def test_delete_record(data_table, data_table_with_data):
+    """
+    Test the delete_by_id function.
+    :param data_table: mock table.
+    :param data_table_with_data: mock table with data.
+    """
+    # set the id to be the same as the mock table in the fixture.
+    db_id = 'abcd-1234'
+
+    # create the dynamo db object.
+    obj_db = DynamoDb()
+
+    # overwrite the table with the mock table.
+    obj_db.table = data_table_with_data
+
+    # run the test
+    assert obj_db.delete_by_id(db_id) is True
